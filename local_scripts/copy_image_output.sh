124e4abbfc3c5307357a08d1d3e0b83beee2cbd3
Maksym Sutkovenko <msutkovenko@readdle.com>
2023-06-24 14:04:46 +0300

    Now local image_output archive is unzipped

--- local_scripts/copy_image_output.sh
+++ local_scripts/copy_image_output.sh
@@ -1,1 +1,7 @@
 scp -i $1 ubuntu@$2:~/Cameraman/image_output.zip ~/Desktop/
+
+timestamp=$(date +%Y%m%d%H%M%S)
+unzip_dir="$HOME/Desktop/image_output_$timestamp"
+
+# Unzip the archive
+unzip ~/Desktop/image_output.zip -d "$unzip_dir"
